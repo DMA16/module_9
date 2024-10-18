1d4ed2693e8e077eb5cd573aa1d516d9d0d87942
Oz <oz.5.1.14.10.18@gmail.com>
2024-10-19 01:57:14 +0300

    Added module_9_6.py

---
+++ module_9_6.py
@@ -0,0 +1,16 @@
+from module_9_3 import second
+
+
+def all_variants(text):
+    for i in range(len(text)):
+        first = 0
+        second = i
+        while second < len(text):
+            yield text[first:second + 1]
+            first, second = first + 1, second + 1
+
+
+
+a = all_variants("abcd")
+for i in a:
+    print(i)
